fd5e5a9a276aa3df2cc966acb39cdffdce3cd75f
Shunleiii <shunlei1222006@gmail.com>
2025-05-20 14:18:04 +0630

    concat variable expression have finished

---
+++ arrayTest.php
@@ -0,0 +1,22 @@
+<?php
+
+$str = "A";
+$str[2] = "s";
+$str[1] = "u";
+$str = $str."t"; //concatenate str var and string literal
+print $str. "<br>";
+
+$name = "John";
+$$name = "Registered user";
+print $John. "<br>";
+
+$name = 'Rob';
+echo $name. "<br>";
+$a = "Hello";
+$b = $a."World!";
+echo $b;
+
+$a = 4;
+$b = 2;
+
+?>
